e2ee1e5e3b581e3bef60bfe2564bd1f820635824
SahayakSharma <sahayaksharma6@gmail.com>
2025-04-11 16:41:55 +0530

    port settings change

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { mainRouter } from "./Routes";
 import dotenv from "dotenv"
 dotenv.config()
 const app=express();
-const PORT=process.env.PORT || 3000
+const PORT=process.env.CLIENTCHATPORT || 5001
 
 app.use(cors())
 app.use(express.json())
